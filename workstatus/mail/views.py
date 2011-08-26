c91b3306c9df1e85e173b79362690a4bbdf3d24d
Priscilla <priscilla@myplanetdigital.com>
2011-08-26 13:43:31 -0400

    nothing happened

--- workstatus/mail/views.py
+++ workstatus/mail/views.py
@@ -83,7 +83,7 @@ def parser(request):
                 #print projects[i]
                 start = x+1
                 x = find(tempString, '#', start)
-    output = "This is the message: \n" + tempString
+    output = "This is the message: " + tempString
     #This is the message:<br>
     #These are the projects added
     #print startingPros
