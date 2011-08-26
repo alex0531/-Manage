6637740879af3713a4cdd844e43489f121a28c1c
Priscilla <priscilla@myplanetdigital.com>
2011-08-26 15:18:05 -0400

    fixed bracket

--- workstatus/mail/views.py
+++ workstatus/mail/views.py
@@ -87,7 +87,7 @@ def parser(request):
                 x = find(tempString, '#', start)
 
 
-    template = get_template{'testing.html')
+    template = get_template{'testing.html'}
     
     variables = Context({'startingPros': startingPros, 'doingPros': doingPros, 'donePros': donePros, 'pausePros': pausePros})
     
