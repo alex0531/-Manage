e9d7e557395dd0b6acd8f3446bd04425bfcd3af6
Priscilla <priscilla@myplanetdigital.com>
2011-08-29 13:23:05 -0400

    trying to fix invalid syntax in urls.py

--- workstatus/urls.py
+++ workstatus/urls.py
@@ -15,8 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^$', parser),
-    (r'^sendemail/$',sendEmail('priscilla@myplanetdigital.com'),
-
-   
-
+    (r'^sendemail/',sendEmail('priscilla@myplanetdigital.com'),
 )
