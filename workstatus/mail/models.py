d00e284dfe17af31749c544c21df56ba9ee1ddb3
Priscilla <priscilla@myplanetdigital.com>
2011-08-30 10:04:14 -0400

    user or email address

--- workstatus/mail/models.py
+++ workstatus/mail/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Project(models.Model):
-    user = models.ForeignKey(User)
+    #user = models.ForeignKey(User)
+    user = models.CharField(max_length = 200)
     name = models.CharField(max_length =200)
     progress = models.IntegerField()
