2eeab583c64d4d5afa302c5bc062008659eca290
Priscilla <priscilla@myplanetdigital.com>
2011-09-01 12:44:32 -0400

    trying to link username to user pages

--- workstatus/urls.py
+++ workstatus/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^$', today),
-    #(r'^parser/',parser)
     (r'^$', read),
     (r'^updates/',parser),
     (r'^(\w+)/$', user_page),
+)
