1834b04de2ba52e3ed419d1cbb380a1dc1a76e5e
Alex Lee <alexyalmighty@hotmail.com>
2011-09-01 13:41:36 -0700

    w.e

--- workstatus/urls.py
+++ workstatus/urls.py
@@ -16,7 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^$', sendMorningMail),
+    (r'^Morning/$', sendMorningMail),
+    (r'^$', read),
     (r'^reminder/$', sendReminderMail),
     (r'^updates/$',parser),
     (r'^updates/(\w+)/$', user_page),
