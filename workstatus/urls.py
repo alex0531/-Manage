5dac2ea7fe1968957a34aa39b2a6c211cedc9f28
Priscilla <priscilla@myplanetdigital.com>
2011-09-01 14:10:25 -0400

    urls

--- workstatus/urls.py
+++ workstatus/urls.py
@@ -17,7 +17,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^$', read),
-    (r'^reminder/$', sendReminderMail),
     (r'^updates/$',parser),
     (r'^updates/(\w+)/$', user_page),
 )
