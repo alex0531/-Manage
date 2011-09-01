73677d5479776701d5f34f942262229fce4fca3b
Alex Lee <alexyalmighty@hotmail.com>
2011-09-01 13:20:50 -0700

    changed something

--- workstatus/sendingMassEmail/views.py
+++ workstatus/sendingMassEmail/views.py
@@ -9,6 +9,7 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives #
 from django.template.loader import render_to_string #
 from django.utils.html import strip_tags #
+from workstatus.mail.models import *
 import feedparser
 
 import os, time
@@ -16,15 +17,15 @@ from datetime import date
 
     
     
-def today(request):
-    today = date.today()
-
-    while True:
-        if today.isoweekday() < 6:
-            if str(time.strftime('%X')) == '12:21:00':
-                sendMorningMail(request)
-                time.sleep(1)
-                
+#def today(request):
+#    today = date.today()
+#
+#    while True:
+#        if today.isoweekday() < 6:
+#            if str(time.strftime('%X')) == '12:21:00':
+#                sendMorningMail(request)
+#                time.sleep(1)
+#                
     #    elif today.isoweekday() < 6:
     #        if str(time.strftime('%X')) == '12:16:00':
     #            sendReminderMail(request)
@@ -46,22 +47,28 @@ def sendMorningMail(request):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
     
+    return HttpResponse()    
     
  
-#def sendReminderMail(request):
-#    template = get_template('Reminder_Mail.html')
-#    
-#    subject, from_email, to = 'Reminder Email (Reply to this Emailaddress ONLY!)', 'umanage.mpd@gmail.com', 'alex@myplanetdigital.com'
-#
-#previous = Message.objects.all()[0].content
-
-#    html_content = render_to_string('Reminder_Mail.html')
-#    text_content = strip_tags(html_content) #this strips the html, so people will have the text as well
-#    
-#    #Create the email, and attach the HTML version as well.
-#    
-#    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-#    msg.attach_alternative(html_content, "text/html")
-#    msg.send()
-#    
-#    return HttpResponse()
+def sendReminderMail(request):
+    for user in User.objects.all():
+        user_msgs = list(Message.objects.filter(user=user))
+        
+        #ssort by time
+        
+        if len(user_msgs) >0:        
+            previous = user_msgs[0].content
+                
+            template = get_template('Reminder_Mail.html')
+            
+            subject, from_email, to = 'Reminder Email (Reply to this Emailaddress ONLY!)', 'umanage.mpd@gmail.com', str(user.email) 
+            html_content = render_to_string('Reminder_Mail.html',{'previous':previous})
+            text_content = strip_tags(html_content) #this strips the html, so people will have the text as well
+            
+            #Create the email, and attach the HTML version as well.
+            
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+        
+    return HttpResponse()
--- workstatus/urls.py
+++ workstatus/urls.py
@@ -16,8 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^$', today),
-    (r'^$', read),
+    (r'^$', sendMorningMail),
+    (r'^reminder/$', sendReminderMail),
     (r'^updates/$',parser),
     (r'^updates/(\w+)/$', user_page),
 )
